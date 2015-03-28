d5e731786570ee4d2dc64e1c9b0c1dec69b74e3b
Toru Matsuoka <lettas0726@gmail.com>
2015-03-28 23:52:57 +0900

    データを別ファイルに分ける

---
+++ apkallu/data/sightseeing.js
@@ -0,0 +1,4 @@
+module.exports = [
+  {no: 1, name: 'ある商人が見た景色', zone: 'リムサ・ロミンサ：上甲板層', coordX: 9, coordY: 7, openedAt: '08:00', closedAt: '12:00', weather: '晴れ', emote: '見渡す', comment: ''},
+  {no: 2, name: 'ある海賊が見た景色', zone: 'リムサ・ロミンサ：下甲板層', coordX: 7, coordY: 15, openedAt: '18:00', closedAt: '05:00', weather: '快晴', emote: '見渡す', comment: ''}
+]
--- apkallu/main.js
+++ apkallu/main.js
@@ -33,9 +33,9 @@ var main = new Vue({
     gridColumnName: {
       no: 'No', name: 'タイトル', zone: '場所', time: '時間帯', weather: '天候', emote: 'エモート', comment: '備考'
     },
-    gridData: [
-      {no: 1, name: 'ある商人が見た景色', zone: 'リムサ・ロミンサ：上甲板層', coordX: 9, coordY: 7, openedAt: '08:00', closedAt: '12:00', weather: '晴れ', emote: '見渡す', comment: ''},
-      {no: 2, name: 'ある海賊が見た景色', zone: 'リムサ・ロミンサ：下甲板層', coordX: 7, coordY: 15, openedAt: '18:00', closedAt: '05:00', weather: '快晴', emote: '見渡す', comment: ''},
-    ]
+    gridData: require('./data/sightseeing.js')
   }
 });
+
+sightseeing = require('./data/sightseeing.js');
+
