9a2925aa27d1a0b6b2f5c897f69cf9c82957924d
Toru Matsuoka <lettas0726@gmail.com>
2015-03-30 00:20:18 +0900

    クリックで完了

--- apkallu/data-table/index.js
+++ apkallu/data-table/index.js
@@ -1,26 +1,30 @@
 require('insert-css')(require('./style.css'))
 module.exports = {
-  _savekey: 'apkallu.userdata',
   template: require('./template.html'),
   replace: true,
 
   compiled: function() {
     var self = this;
+    var SAVE_KEY = 'apkallu.userdata';
 
-    function loadUserdata() {
-      var savedata = JSON.parse(localStorage.getItem(self._savekey));
+    (function initialize(){
+        self.data.forEach(function(row) {
+            self.userdata.completed.$add(row.no, false);
+        });
+    })();
+
+    (function loadUserdata() {
+      var savedata = JSON.parse(localStorage.getItem(SAVE_KEY));
       var userdata = self.userdata;
       mergeObjects(userdata, savedata);
       self.userdata = userdata;
-    }
 
-    function mergeObjects(o1, o2) {
-      for(var key in o2) {
-        o1[key] = o2[key];
+      function mergeObjects(o1, o2) {
+        for(var key in o2) {
+          o1[key] = o2[key];
+        }
       }
-    }
-
-    loadUserdata();
+    })();
 
     this.$watch('userdata.extention.shows', function(showsExtention) {
         if (showsExtention) {
@@ -33,8 +37,15 @@ module.exports = {
 
     this.$watch('userdata', function(userdata) {
         var savedata = JSON.stringify(userdata);
-        localStorage.setItem(self._savekey, savedata);
+        localStorage.setItem(SAVE_KEY, savedata);
     }, true);
+  },
+
+  methods: {
+    toggleCompleted: function(row) {
+      var origin = this.userdata.completed[row.no];
+      this.userdata.completed[row.no] = !origin;
+    }
   },
 
   filters: {
@@ -54,6 +65,7 @@ module.exports = {
       userdata: {
         extention: { shows: false, limit: 20 },
         visible: { zone: false, coord: false, time: false, weather: false, emote: false, comment: false },
+        completed: {},
       },
       data: require('../data/sightseeing.js')
     };
