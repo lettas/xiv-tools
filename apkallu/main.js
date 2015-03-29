5a8cb1968acab9cfc32327e29528f1060912afd1
Toru Matsuoka <lettas0726@gmail.com>
2015-03-30 00:48:39 +0900

    ヘッダーとフッターをindex.htmlにマージ

--- apkallu/main.js
+++ apkallu/main.js
@@ -2,9 +2,7 @@ require('insert-css')(require('./style.css'))
 var main = new Vue({
   el: '#main',
   components: {
-    header: require('./header'),
     dataTable: require('./data-table'),
-    footer: require('./footer'),
   }
 });
 
