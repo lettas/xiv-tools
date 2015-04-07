75e25408f9302dbe9a15c037610425ffc252e108
Toru Matsuoka <lettas0726@gmail.com>
2015-04-07 16:32:09 +0900

    gulpの引数とれた

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,19 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var watchify = require('watchify');
 
-var target = 'cactpot';
+var target = gutil.env.target || 'cactpot';
 
 function targetFilePath(target) {
   return './' + target + '/main.js';
 }
 
 gulp.task('build', function() {
+    gutil.log('build target: ' + target);
     rebundle(target, browserify(targetFilePath(target)));
 });
 
 gulp.task('watch', function () {
+    gutil.log('watch target: ' + target);
     var bundler = watchify(browserify(targetFilePath(target)), watchify.args);
     bundler.rebundle = function() { rebundle(target, bundler); };
     bundler.on('update', bundler.rebundle);
